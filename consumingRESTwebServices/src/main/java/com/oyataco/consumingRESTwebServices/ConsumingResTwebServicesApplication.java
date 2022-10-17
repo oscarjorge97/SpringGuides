9dfccb807328fa67e89a222bceb278c016bd87c0
Oscar Yataco <oscarjorge97@gmail.com>
2022-10-17 22:10:03 +0200

    Añadiendo primer Servicio web RESTful con Spring

---
+++ consumingRESTwebServices/src/main/java/com/oyataco/consumingRESTwebServices/ConsumingResTwebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.oyataco.consumingRESTwebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingResTwebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingResTwebServicesApplication.class, args);
+	}
+
+}
---
+++ consumingRESTwebServices/src/test/java/com/oyataco/consumingRESTwebServices/ConsumingResTwebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyataco.consumingRESTwebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingResTwebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
