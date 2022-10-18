99402bc19cbf357ccbc8c6e2befdccfeae074964
Oscar Yataco <oscarjorge97@gmail.com>
2022-10-18 17:48:23 +0200

    Creando proyectos con Maven

---
+++ buildingJavaProjectsWithMaven/src/main/java/Hello/Greeter.java
@@ -0,0 +1,10 @@
+package Hello;
+
+public class Greeter {
+
+    //method
+    public String sayHello(){
+        return "Hello world";
+    }
+}
+
---
+++ buildingJavaProjectsWithMaven/src/main/java/Hello/GreeterTest.java
@@ -0,0 +1,18 @@
+package Hello;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GreeterTest {
+    private Greeter greeter=new Greeter();
+
+    @Test
+    public void greeterSaysHello(){
+        assertThat(greeter.sayHello(),containsString("Aloha"));
+    }
+
+
+
+
+}
